18554888098136043769e9802c85f8866f8d18e0
Raymond Wilfong <rwilfong@rewnet.com>
2024-06-03 20:10:34 -0500

    Fixed compiler and debugger. Complete startup project (MyProject) that can be duplicated to make a new project with all the libraries and configuration ready.

--- MyProject/src/main.c
+++ MyProject/src/main.c
@@ -1,17 +1,41 @@
-/* Includes ------------------------------------------------------------------*/
 #include "stm8s.h"
-#include "stm8s_conf.h" // select components needed for build - included here to find it quickly
-#include "stm8s_it.h" // interrupt vector map
+#include "stm8s_it.h"    /* SDCC patch: required by SDCC for interrupts */
 
-// private includes
-#include "clock.h" // clock configuration functions
+#define LED_GPIO_PORT  (GPIOB)
+#define LED_GPIO_PINS  (GPIO_PIN_5)	
 
+void Delay (uint16_t nCount);
 
 void main(void)
 {
-    clock_config();
-    while (1)
-    {
+  const int flashCount = 2; // how many flashes before pause?
+
+  /* Initialize I/Os in Output Mode */
+  GPIO_Init(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS, GPIO_MODE_OUT_PP_LOW_FAST);
+
+
+  while (1)
+  {
+    /* Toggles LEDs */
+    for (int i = 0; i < 2 * flashCount; ++i) {
+      GPIO_WriteReverse(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS);
+      Delay(0xFFFF);
     }
+    Delay(0xFFFF);
+    Delay(0xFFFF);
+  }
+
 }
 
+void Delay(uint16_t nCount)
+{
+  /* Decrement nCount value */
+  while (nCount != 0)
+  {
+    nCount--;
+  }
+}
+
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
